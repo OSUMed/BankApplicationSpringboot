bf06dae64152de87d47763b24cda230627dd865c
OSUMed <62990411+OSUMed@users.noreply.github.com>
2023-09-18 22:17:20 -0700

    clean up controller file: remove comments & format

--- src/main/java/com/coderscampus/assignment13/web/UserController.java
+++ src/main/java/com/coderscampus/assignment13/web/UserController.java
@@ -1,6 +1,5 @@
 package com.coderscampus.assignment13.web;
 
-import java.util.Arrays;
 import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,70 +19,57 @@ public class UserController {
 
 	@Autowired
 	private UserService userService;
-	
+
 	@Autowired
 	private AccountService accountService;
 
 	@GetMapping("/register")
-	public String getCreateUser (ModelMap model) {
-
+	public String getCreateUser(ModelMap model) {
 		model.put("user", new User());
-
 		return "register";
 	}
 
 	@PostMapping("/register")
-	public String postCreateUser (User user) {
+	public String postCreateUser(User user) {
 		System.out.println(user);
 		userService.saveUser(user);
-		accountService.setAccountAmount(accountService.getAccountAmount()+2);
+		accountService.setAccountAmount(accountService.getAccountAmount() + 2);
 		return "redirect:/register";
 	}
 
 	@GetMapping("/users")
-	public String getAllUsers (ModelMap model) {
+	public String getAllUsers(ModelMap model) {
 		Set<User> users = userService.findAll();
-
 		model.put("users", users);
 		if (users.size() == 1) {
 			model.put("user", users.iterator().next());
 		}
-
 		return "users";
 	}
 
 	@GetMapping("/users/{userId}")
-	public String getOneUser (ModelMap model, @PathVariable Long userId) {
-		System.out.println("User id is: "+userId);
+	public String getOneUser(ModelMap model, @PathVariable Long userId) {
 		User user = userService.findById(userId);
-
-//		model.put("users", Arrays.asList(user));
 		model.put("user", user);
-//		Integer new_account_number = accountService.getNumberOfAccounts();
 		Integer nextAccountNumber = accountService.getNextAccountNumber();
-		System.out.println("total accounts are: "+ nextAccountNumber);
 		model.put("new_account_number", nextAccountNumber);
 		return "user";
 	}
 
 	@PostMapping("/users/{userId}")
-	public String postOneUser (User user) {
-		System.out.println("updating user is... "+user);
+	public String postOneUser(User user) {
 		userService.saveUser(user);
-		return "redirect:/users/"+user.getUserId();
+		return "redirect:/users/" + user.getUserId();
 	}
 
 	@PostMapping("/users/{userId}/delete")
-	public String deleteOneUser (@PathVariable Long userId) {
+	public String deleteOneUser(@PathVariable Long userId) {
 		userService.delete(userId);
 		return "redirect:/users";
 	}
-	
+
 	@GetMapping("/users/{userId}/accounts/{accountId}")
-	public String getAUserAccountPage (ModelMap model, @PathVariable Long userId, @PathVariable Long accountId) {
-		System.out.println("In update addAccountToUser");
-		System.out.println("updating account is... "+ accountId);
-		System.out.println("updating account userId is... "+ userId);
+	public String getAUserAccountPage(ModelMap model, @PathVariable Long userId, @PathVariable Long accountId) {
 		User user = userService.findById(userId);
 		Account account = accountService.getAccount(accountId);
 		if (account.getAccountId() == null) {
@@ -94,26 +80,16 @@ public class UserController {
 		model.put("new_account_number", accountId);
 		return "account";
 	}
-	
+
 	@PostMapping("/users/{userId}/accounts/{accountId}")
-	public String updateAccountToUser (Account account, @PathVariable Long userId, @PathVariable Long accountId) {
-		System.out.println("In update addAccountToUser");
-		System.out.println("updating account is... "+ account);
-		System.out.println("updating account userId is... "+ userId);
-		System.out.println("updating account accountId is... "+ accountId);
+	public String updateAccountToUser(Account account, @PathVariable Long userId, @PathVariable Long accountId) {
 		accountService.saveAccount(account, userId);
-		String redirectString =  "redirect:/users/"+ userId + "/accounts/" + accountId;
-		System.out.println("redirect string is: " + redirectString);
-//		return "redirect:/users/"+ userId;
-		return "redirect:/users/"+ userId + "/accounts/" + accountId;
+		return "redirect:/users/" + userId + "/accounts/" + accountId;
 	}
-	
+
 	@PostMapping("/users/{userId}/accounts")
-	public String addAccountToUser (Account account, @PathVariable Long userId) {
-		System.out.println("In update addAccountToUser");
-		System.out.println("updating account is... "+ account);
-		System.out.println("updating account userId is... "+ userId);
+	public String addAccountToUser(Account account, @PathVariable Long userId) {
 		accountService.saveAccount(account, userId);
-		return "redirect:/users/"+ userId;
+		return "redirect:/users/" + userId;
 	}
 }
